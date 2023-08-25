fefae64ddb91987dce8f9b03780568902f711c44
JohanMallqui <106215428+johanmmalqui@users.noreply.github.com>
2023-08-26 00:39:39 +0900

    commit

--- file-tree.py
+++ file-tree.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("path")
 
 args = parser.parse_args()
-target_dir = Path("C:\\dev\\langs\\py")
+target_dir = Path(args.path)
 
 
 if not target_dir.exists():
